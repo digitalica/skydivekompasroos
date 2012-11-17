2b1557af37b22c22440af9ff0ddc23b63ef46462
Robbert Wethmar <robbert@digitalica.nl>
2012-11-17 10:29:41 +0100

    fixed parachute data, added manufacturers and urls

--- src/nl/digitalica/skydivekompasroos/CanopyListActivity.java
+++ src/nl/digitalica/skydivekompasroos/CanopyListActivity.java
@@ -45,13 +45,13 @@ public class CanopyListActivity extends KompasroosBaseActivity {
 							.getAttributeValue(null, "category");
 					int canopyCategory;
 					canopyCategory = Integer.parseInt(canopyCategoryString);
-					String canopyMaker = canopiesParser.getAttributeValue(null,
-							"maker");
+					String canopyManufacturer = canopiesParser
+							.getAttributeValue(null, "manufacturer");
 					String canopyName = canopiesParser.getAttributeValue(null,
 							"name");
 
-					canopyList.add(new Canopy(canopyCategory, canopyMaker,
-							canopyName));
+					canopyList.add(new Canopy(canopyCategory,
+							canopyManufacturer, canopyName));
 				}
 
 			}
@@ -66,7 +66,7 @@ public class CanopyListActivity extends KompasroosBaseActivity {
 			}
 		}
 
-		// sort the canopyList on type, name, maker
+		// sort the canopyList on type, name, manufacturer
 		Collections.sort(canopyList, new CanopyComparator());
 
 		Bundle extras = getIntent().getExtras();
@@ -180,7 +180,7 @@ public class CanopyListActivity extends KompasroosBaseActivity {
 		textView.setTypeface(null, Typeface.BOLD);
 		row.addView(textView);
 		textView = new TextView(this);
-		textView.setText(getString(R.string.canopyMaker));
+		textView.setText(getString(R.string.canopyManufacturer));
 		textView.setTypeface(null, Typeface.BOLD);
 		row.addView(textView);
 		canopyTable.addView(row);
@@ -197,24 +197,26 @@ public class CanopyListActivity extends KompasroosBaseActivity {
 		textView.setText(theCanopy.name);
 		row.addView(textView);
 		textView = new TextView(this);
-		textView.setText(theCanopy.maker);
+		textView.setText(theCanopy.manufacturer);
 		row.addView(textView);
 
 		// add row to table
 		canopyTable.addView(row);
-		skydiveKompasroosResult.append(theCanopy.name + " - " + theCanopy.maker
-				+ System.getProperty("line.separator"));
+		skydiveKompasroosResult
+				.append(theCanopy.name + " - " + theCanopy.manufacturer
+						+ System.getProperty("line.separator"));
 
 	}
 
 	private class Canopy {
 		public int category;
-		public String maker;
+		public String manufacturer;
 		public String name;
 
-		public Canopy(int canopyCategory, String canopyMaker, String canopyName) {
+		public Canopy(int canopyCategory, String canopyManufacturer,
+				String canopyName) {
 			this.category = canopyCategory;
-			this.maker = canopyMaker;
+			this.manufacturer = canopyManufacturer;
 			this.name = canopyName;
 		}
 
@@ -230,7 +232,7 @@ public class CanopyListActivity extends KompasroosBaseActivity {
 			int result = c1.name.compareTo(c2.name);
 			if (result != 0)
 				return result;
-			return c1.maker.compareTo(c2.maker);
+			return c1.manufacturer.compareTo(c2.manufacturer);
 		}
 
 	}
