ab04646d3d55491f60776f5253ef7ff679ddb47c
Robbert Wethmar <robbert@digitalica.nl>
2013-09-23 16:37:14 +0200

    extra information in publish.sh

--- publish.sh
+++ publish.sh
@@ -4,6 +4,12 @@
 #
 # NOTE: paths probably need to be checked/changed depending on local installation!!!!
 
+
+# To export an unsigned APK from Eclipse, right-click the project in the Package Explorer
+# and select Android Tools > Export Unsigned Application Package. Then specify the file
+# location for the unsigned APK. (Alternatively, open your AndroidManifest.xml file in
+# Eclipse, select the Manifest tab, and click Export an unsigned APK.)
+
 cd /home/robbert
 
 jarsigner -verbose -sigalg MD5withRSA -digestalg SHA1 -keystore digitalica.keystore SkydiveKompasroos.apk digitalica
